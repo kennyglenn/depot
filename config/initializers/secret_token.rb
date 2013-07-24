94e7df26bbfc198917068727e037cbbc7b419e92
Kenny Glenn <kglenn@topicdesign.com>
2013-07-24 12:35:59 -0400

    Creating Rails project
    rails, scafollded product, added images, added stylesheets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '06c9eddae27c0d7944b613f72b5c4b17bd90a608f6907ea2cdf70c6177bc846efc9143f4554be87efd31f2491149dde94abdc1494b96cb9c5b54efc00f58053f'
