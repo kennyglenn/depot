d9b1d804219a12c8f743846f83aa04723ad4d083
Kenny Glenn <kenny@idowork.com>
2013-07-26 08:21:38 -0400

    added wireframe stylesheet, edited images path in seeds file (did not need /images/), scafollded the cart and set up user session for shopping cart

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select 'h3', 'Programing Ruby 1.9'
+    assert_select '.price' , /\$[,\d]+\.\d\d/
   end
 
 end
