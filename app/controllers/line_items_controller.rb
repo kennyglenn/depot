4af6d63586889b5ca1da4cc558d05deb347a82aa
Kenny Glenn <kenny@idowork.com>
2013-08-03 13:46:19 -0400

    removed pry, linked image on index

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,7 +40,6 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(:product => product)
