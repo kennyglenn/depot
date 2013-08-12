74bc2d55f72dfed491a761f1fd813a5e88bd9d41
Kenny Glenn <kenny@idowork.com>
2013-08-12 18:38:49 -0400

    added quantities, edited sidebar links

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,17 @@
 class Cart < ActiveRecord::Base
   attr_accessible :title, :body
   has_many :line_items, :dependent => :destroy
+  
+  
+  # Add Product
+  # Checks for quantity to add 1 if it is not new
+  def add_product(product_id)
+    current_item = line_items.where(:product_id => product_id).first
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(:product_id => product_id)
+    end
+    current_item
+  end
 end
