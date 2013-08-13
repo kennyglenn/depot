6f1de58442f82cc24653686fe028a05c239b0b01
Kenny Glenn <kenny@idowork.com>
2013-08-13 11:58:35 -0400

    added migration to combine line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   has_many :line_items
   
   before_destroy :ensure_not_referenced_by_any_line_item
+  
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130803174853) do
+ActiveRecord::Schema.define(:version => 20130813122542) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
